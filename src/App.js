4619d2782c7cdbee1b4407c2e959a5e93f0f3076
EnzoDuhalde <enzo-duhalde@hotmail.fr>
2019-04-11 14:28:18 +0200

    finished quest react-router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, BrowserRouter, Switch, NavLink } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./History";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <BrowserRouter>
+                        <div>
+                            <div>
+                                <NavLink
+                                    exact
+                                    to="/"
+                                    activeStyle={{
+                                        color: "Yellow",
+                                        fontWeight: "Bold"
+                                    }}
+                                >
+                                    {" "}
+                                    Home{" "}
+                                </NavLink>
+                            </div>
+                            <div>
+                                <NavLink to="/Contact" activeClassName="selected">
+                                    {" "}
+                                    History
+                                </NavLink>
+                            </div>
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                <Route path="/Contact" component={Contact} />
+                            </Switch>
+                        </div>
+                    </BrowserRouter>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
